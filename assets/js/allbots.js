f571361dcde974aad65f94d3fec7bd889a05d053
niksteel <nikolassteel@gmail.com>
2015-07-17 03:37:25 -0500

    pre-next refactoring.

--- assets/js/allbots.js
+++ assets/js/allbots.js
@@ -16,60 +16,55 @@ var savePlayerScope = function (Player) {
 window._window = Object.keys(window);
 
 var Bots = {
-
-   allBotList: [],
    Player1: null,
    Player2: null,
 
    init: function() {
-      Bots.allBotList.push(Bots.ex1);
-      Bots.allBotList.push(Bots.ex2);
       Bots.setPlayers(1,2);
    },
 
-
-    
    setPlayers: function(A,B) {
-      if ((A > 0) && (B > 0) && (A <= Bots.allBotList.length) && (B <= Bots.allBotList.length)){
-         Bots.Player1 = Bots.allBotList[A-1];
-         Bots.Player2 = Bots.allBotList[B-1]; 
+      if ((A > 0) && (B > 0) && (A <= allBotList.length) && (B <= allBotList.length)){
+         Bots.Player1 = allBotList[A-1];
+         Bots.Player2 = allBotList[B-1]; 
       }
    },
+};
 
-   ex1: {
-      name: "donothing",
-      
-      new_game: function() {
-      },
-
-      makeMove: function() {
-         return PASS;
-      }
+var  nothingBot = {
+   name: "nothingBot",
+   
+   new_game: function() {
    },
 
-   ex2: {
-      name: "randombot",
-      
-      new_game: function() {
-      },
-
-      makeMove: function() {
-         var board = get_board();
-
-         // we found an item! take it!
-         if (board[get_my_x()][get_my_y()] > 0) {
-             return TAKE;
-         }
-
-         var rand = Math.random() * 4;
-
-         if (rand < 1) return NORTH;
-         if (rand < 2) return SOUTH;
-         if (rand < 3) return EAST;
-         if (rand < 4) return WEST;
-
-         return PASS;
-      }
+   makeMove: function() {
+      return PASS;
    }
 };
 
+var randomBot = {
+   name: "randombot",
+   
+   new_game: function() {
+   },
+
+   makeMove: function() {
+      var board = get_board();
+
+      // we found an item! take it!
+      if (board[get_my_x()][get_my_y()] > 0) {
+          return TAKE;
+      }
+
+      var rand = Math.random() * 4;
+
+      if (rand < 1) return NORTH;
+      if (rand < 2) return SOUTH;
+      if (rand < 3) return EAST;
+      if (rand < 4) return WEST;
+
+      return PASS;
+   }
+};
+
+var allBotList = [nothingBot, randomBot];
---
+++ newbots/nothingBot.txt
@@ -0,0 +1,10 @@
+nothingBot: {
+   name: "nothingBot",
+   
+   new_game: function() {
+   },
+
+   makeMove: function() {
+      return PASS;
+   }
+}

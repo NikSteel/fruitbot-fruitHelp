8efca1658f442b5e65025742a468b2b6127b08c3
niksteel <nikolassteel@gmail.com>
2015-07-17 22:56:35 -0500

    fixed get_my_item_count and get_opponent_item_count bug and refactored myBotCollected and simpleBotCollected as myCollected and oppCollected respectively

--- bots/player1.js
+++ bots/player1.js
@@ -6,6 +6,6 @@ function new_game() {
 }
 
 function make_move() {
-   alert("my name is " + name + "my fruit counts are type1: " + get_my_item_count(1) + ", type 2: " + get_my_item_count(2) + ", type 3: " + get_my_item_count(3) + ", type 4: " + get_my_item_count(4) + ". My opponent's fruit counts are type1: " + get_opponent_item_count(1) + ", type 2: " + get_opponent_item_count(2) + ", type 3: " + get_opponent_item_count(3) + ", type 4: " + get_opponent_item_count(4)); 
+   alert("my name is " + name + ", my fruit counts are type1: " + get_my_item_count(1) + ", type 2: " + get_my_item_count(2) + ", type 3: " + get_my_item_count(3) + ", type 4: " + get_my_item_count(4) + ". My opponent's fruit counts are type1: " + get_opponent_item_count(1) + ", type 2: " + get_opponent_item_count(2) + ", type 3: " + get_opponent_item_count(3) + ", type 4: " + get_opponent_item_count(4)); 
    return PASS;
 }
